a86052874c550d3d13e80d398cf0fb9c83c8f503
Eric McNiece <emcniece@gmail.com>
2019-02-05 16:54:43 -0800

    Prettier, ESLint, Grid page setup

---
+++ src/components/GridDemo.vue
@@ -0,0 +1,62 @@
+<template>
+  <div>
+    <h1>{{ msg }}</h1>
+    <ul>
+      <li>
+        <router-link to="/">Home</router-link>
+      </li>
+    </ul>
+
+    <div class="row">
+      <div
+        class="col-xs-12
+              col-sm-8
+              col-md-6
+              col-lg-4"
+      >
+        <div class="box">col-xs-12 col-sm-8 col-md-6 col-lg-4</div>
+      </div>
+    </div>
+
+    <div class="row">
+      <div class="col-xs-2">
+        <div class="box">col-xs-2</div>
+      </div>
+
+      <div class="col-xs-2">
+        <div class="box">col-xs-2</div>
+      </div>
+
+      <div class="col-xs-2">
+        <div class="box">col-xs-2</div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "GridDemo",
+  data() {
+    return {
+      msg: "Grid Demo"
+    };
+  }
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+.box {
+  background: rgba(0, 0, 0, 0.2);
+  border: 1px solid rgba(0, 0, 0, 0.4);
+}
+</style>
